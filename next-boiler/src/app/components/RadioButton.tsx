8da88751f74193f2031b7172a077a8c60c1dbc6e
sarahp27 <sarah.patel@xloopdigital.com>
2025-02-12 12:35:40 +0500

    Theme including fonts and color scheme is added. Radio button is added

---
+++ next-boiler/src/app/components/RadioButton.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+interface RadioOption {
+    label: string;
+    value: string;
+}
+
+interface RadioButtonProps {
+    name: string;
+    dimension?: string;
+    options: RadioOption[];
+    selectedValue: string;
+    onChange: (value: string) => void;
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({
+    name,
+    dimension = "flex-col",
+    options,
+    selectedValue,
+    onChange,
+}) => {
+    return (
+        <div className={`flex ${dimension}`}>
+            {options.map((option) => (
+                <label
+                    key={option.value}
+                    className="flex items-center space-x-2 cursor-pointer text-surface-400"
+                >
+                    <input
+                        type="radio"
+                        name={name}
+                        value={option.value}
+                        checked={selectedValue === option.value}
+                        onChange={() => onChange(option.value)}
+                        className="hidden"
+                    />
+                    <div
+                        className={`w-5 h-5 border-2 rounded-full flex items-center justify-center  ${selectedValue === option.value
+                            ? "border-foundation-100 bg-foundation-100"
+                            : "border-foundation-100"
+                            }`}
+                    >
+                        {selectedValue === option.value && (
+                            <div className="w-3 h-3 rounded-full"></div>
+                        )}
+                    </div>
+                    <span className="text-[#07224D]">{option.label}</span>
+                </label>
+            ))}
+        </div>
+    );
+};
+
+export default RadioButton;
--- next-boiler/tailwind.config.ts
+++ next-boiler/tailwind.config.ts
@@ -8,9 +8,55 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        neutral: {
+          100: 'var(--neutral-100)',
+          200: 'var(--neutral-200)',
+          300: 'var(--neutral-300)',
+          400: 'var(--neutral-400)',
+          500: 'var(--neutral-500)',
+        },
+        foundation: {
+          100: 'var(--foundation-100)',
+          200: 'var(--foundation-200)',
+          300: 'var(--foundation-300)',
+        },
+        accent: {
+          100: 'var(--accent-100)',
+          200: 'var(--accent-200)',
+          300: 'var(--accent-300)',
+          400: 'var(--accent-400)',
+          500: 'var(--accent-500)',
+        },
+        surface: {
+          100: 'var(--surface-100)',
+          200: 'var(--surface-200)',
+          300: 'var(--surface-300)',
+          400: 'var(--surface-400)',
+        },
+        white: {
+          100: 'var(--white-100)',
+          200: 'var(--white-200)',
+          300: 'var(--white-300)',
+        },
+        black: {
+          400: 'var(--black-400)',
+        },
+      },
+      fontSize: {
+        mainHeadline: 'var(--headline-bold-25)',
+        subHeadline: 'var(--headline-bold-20)',
+        titleBold: 'var(--title-bold-18)',
+        titleRegular: 'var(--title-regular-18)',
+        bodyRegular20: 'var(--body-regular-20)',
+        bodyRegular18: 'var(--body-regular-18)',
+        bodyRegular16: 'var(--body-regular-16)',
+        bodyRegular14: 'var(--body-regular-14)',
+        bodyRegular12: 'var(--body-regular-12)',
+        label: 'var(--label-bold-18)',
       },
     },
   },
