6c5fd36653526e1245c3bbcaeedf388e6ac96467
Mufaddal Mustafa <mufaddal.mustafa@xloopdigital.com>
2025-02-18 14:05:27 +0500

    app layout added. drawer component created. made it responsive

--- next-boiler/src/app/page.tsx
+++ next-boiler/src/app/page.tsx
@@ -1,49 +1,10 @@
-"use client"
-import React from "react";
-// import Heading from "@/components/Heading";
-// import Typography from "@/components/Typography";
-// import RadioButton from "./components/RadioButton";
-
-// const options = [
-//   { label: "Option 1", value: "option1" },
-//   { label: "Option 2", value: "option2" },
-// ];
+import AppLayout from "@/layouts/AppLayout";
 
 export default function Home() {
-  // const [selected, setSelected] = useState<string>("option1");
-
-  // const handleChange = (value: string) => {
-  //   setSelected(value);
-  // };
-
 
   return (
-    <div className="flex flex-col gap-[32px]">
-
-      {/* <Typography className="w-[390px] h-[140px]">
-        <Heading level="h1" weight="bold">Headline Bold - 25 Poppins</Heading>
-        <Heading level="h2" weight="bold">Headline Bold - 20 Poppins</Heading>
-      </Typography>
-
-      <Typography className="w-[272px] h-[126px]">
-        <Heading level="h2" weight="bold">Title - Bold 18 Poppins</Heading>
-        <Heading level="h2" weight="regular">Title - Regular 18 Poppins</Heading>
-      </Typography>
-
-      <Typography className="w-[311px] h-[264px]">
-        <Heading level="h3" weight="regular">Body - Regular 20 Poppins</Heading>
-        <Heading level="h4" weight="regular">Body - Regular 18 Poppins</Heading>
-        <Heading level="h5" weight="regular">Body - Regular 16 Poppins</Heading>
-        <Heading level="h5" weight="regular">Body - Regular 14 Poppins</Heading>
-        <Heading level="h5" weight="regular">Body - Regular 12 Poppins</Heading>
-      </Typography>
-
-      <Typography  className="w-[258px] h-[75px]">
-       <Heading level="h2" weight="bold">Label - Bold 18 Poppins</Heading>
-      </Typography> */}
-
-
-     
-    </div>
+    <AppLayout>
+      <div></div>
+    </AppLayout>
   );
 }
